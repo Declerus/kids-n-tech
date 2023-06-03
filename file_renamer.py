ae29de4bb99f20c521d856209b25a094958ffa06
Emi <119708279+emi-mari@users.noreply.github.com>
2023-06-02 20:28:13 -0700

    Delete file_renamer.py

--- file_renamer.py
+++
@@ -1,6 +0,0 @@
-import os
-_src = "/path/to/directory/"
-_ext = ".png"
-for i,filename in enumerate(os.listdir(_src)):
-    if filename.endswith(_ext):
-        os.rename(filename, _src+str(i).zfill(3)+_ext)
